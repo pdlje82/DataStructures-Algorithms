149c93270c7fbc8da3c5b560ad8e49449b186a1d
Jan Engelke <jan.engelke@gmx.net>
2019-07-21 22:55:45 +0200

    1st revision of Route Planner Algorithm

--- P3/student_code.py
+++ P3/student_code.py
@@ -3,22 +3,12 @@ import json
 
 def shortest_path(M, start, target):
     print("\nshortest path called")
-    currents = []  # currently open nodes
-    visited = []  # closed nodes
-    # dict with:
-    # - straight s2t distances
-    # - g... path cost so
-    # - h... estimated distance to target
-    # - f = g + h
-    frontier_dict = {}
+    visited = {}  # closed nodes
+    f_dict = {}  # - f = g + h
+    g_dict = {}  # - g... path cost so far
+    h_dict = {}  # - heuristic straight start_2_target distances
+    p_dict = {}  # shows parent for each node
     path = [start]  # shortest path from start to goal
-    # calculate straight line distances from goal point to all other points
-    for point in M.intersections:
-        frontier_dict[point] = {'g': None,
-                                'heuristic_distance': direct_dist(M, point, target),
-                                'f': None,
-                                'parent': None}
-    # print('initialized frontier_dict with:\n ', frontier_dict)
 
     if start == target:
         return path
@@ -26,44 +16,33 @@ def shortest_path(M, start, target):
     # expand from start-point
     current_point = start
     print('- start with current point: ', current_point)
-    frontier_dict[current_point]['g'] = 0  # cost from start is 0
-    frontier_dict[current_point]['f'] = frontier_dict[current_point]['heuristic_distance']
-    # print('- f_dict for start {} is to : {}'.format(start, frontier_dict[start]))
-    # print('\n------------------------------------------------------------------------------')
-    while current_point is not target:
-        print('\nWHILE_loop: current point is {} with neighbors {}'.format(current_point, M.roads[current_point]))
+    g_dict[current_point] = 0
+    h_dict[current_point] = direct_dist(M, current_point, target)
+    f_dict[current_point] = h_dict[current_point]
+    p_dict[current_point] = None
+    while f_dict:
         for point in M.roads[current_point]:
-            if point not in visited and \
-                    point not in currents:
-                currents.append(point)
-            if frontier_dict[point]['f'] is None or \
-                    frontier_dict[point]['g'] > frontier_dict[current_point]['g'] + direct_dist(M, current_point, point):
-                # print('- FOR_loop: f-value for {} is: {} --> writing g-, f-, and parent values in frontier_dict:'.
-                #       format(point, frontier_dict[point]['f']))
+            if point not in f_dict and point not in visited:
+                # Calculate heuristic distance to target
+                h_dict[point] = direct_dist(M, current_point, target)
+
+            if point not in visited and point not in f_dict or \
+                    g_dict[point] > g_dict[current_point] + direct_dist(M, current_point, point):
                 # Calculate the total way-costs g
-                g_point = direct_dist(M, point, current_point) + frontier_dict[current_point]['g']
-                frontier_dict[point]['g'] = round(g_point, 3)
+                g_point = direct_dist(M, point, current_point) + g_dict[current_point]
+                g_dict[point] = round(g_point, 3)
                 # Calculate f costs
-                f_point = frontier_dict[point]['g'] + frontier_dict[point]['heuristic_distance']
-                frontier_dict[point]['f'] = round(f_point, 3)
-                # set parent to actual current_point
-                frontier_dict[point]['parent'] = current_point
-                # print('- FOR_loop: f_dict for point {} changed to : {}\n'.format(point, frontier_dict[point]))
-        visited.append(current_point)
-        # print('- VISIT of current_point {} is now COMPLETE'.format(current_point))
-        # print('-- f_dict for point {} is: {}'.format(current_point, frontier_dict[current_point]))
-        # print('-- current_list is: ', currents)
-        # print('-- visited list is: ', visited)
-        # find min f value in frontier dict
-        min_list = [frontier_dict[point]['f'] for point in currents]
-        current_point = currents[min_list.index(min(min_list))]
-        # print('- new current point is {} with min. f-value of {}'.format(current_point,
-        #                                                                        frontier_dict[current_point]['f']))
-        currents.remove(current_point)
-    visited.append(current_point)
-    print('WHILE_loop end')
-    # print(json.dumps(frontier_dict, indent=1))
-    path = output_path(start, target, frontier_dict)
+                f_point = g_dict[point] + h_dict[point]
+                f_dict[point] = round(f_point, 3)
+                p_dict[point] = current_point
+        visited[current_point] = True
+        current_point = min(f_dict, key=f_dict.get)
+        f_dict.pop(current_point)
+    visited[current_point] = True
+    if path is not []:
+        path = output_path(start, target, p_dict)
+    else:
+        return None
     print(path)
     return path
 
@@ -78,11 +57,11 @@ def direct_dist(M, start, target):
     return round(distance, 3)
 
 
-def output_path(start, target, frontier_dict):
+def output_path(start, target, p_dict):
     path = [target]
     node = target
     while node != start:
-        parent_node = frontier_dict[node]['parent']
+        parent_node = p_dict[node]
         path.append(parent_node)
         node = parent_node
     return path[::-1]
